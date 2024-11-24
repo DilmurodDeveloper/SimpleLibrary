12fb64f16bd8f4beeb600e44e4b942fe98d93370
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2024-11-24 10:49:20 +0500

    MODELS: Add Book, Reader and ReaderBook models

---
+++ SimpleLibrary.Api/Models/Foundations/Reader.cs
@@ -0,0 +1,15 @@
+﻿//==========================
+// Simple Library Management
+//==========================
+
+namespace SimpleLibrary.Api.Models.Foundations
+{
+    public class Reader
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public List<string> Books { get; set; }
+    }
+}
---
+++ SimpleLibrary.Api/Models/Foundations/ReaderBook.cs
@@ -0,0 +1,12 @@
+﻿//==========================
+// Simple Library Management
+//==========================
+
+namespace SimpleLibrary.Api.Models.Foundations
+{
+    public class ReaderBook
+    {
+        public Reader Reader { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
