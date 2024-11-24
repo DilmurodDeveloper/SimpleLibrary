fd53273027780b37aed59a16341ce3d24f123d33
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2024-11-24 11:03:02 +0500

    INFRA: Initialize Unit Test Project

---
+++ SimpleLibrary.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,12 @@
+﻿//==========================
+// Simple Library Management
+//==========================
+
+namespace SimpleLibrary.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
