ece4728c9565e064a649599875bbe3b1cac3e0f7
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2024-11-27 17:33:58 +0500

    DATA: Models & Migrate Books & Readers

---
+++ SimpleLibrary.Api/Models/Foundations/Books/Book.cs
@@ -0,0 +1,19 @@
+﻿//==================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Library Management System
+//==================================================
+
+namespace SimpleLibrary.Api.Models.Foundations.Books
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public Guid ReaderId { get; set; }
+
+        public Book(Guid readerId) =>
+            ReaderId = readerId;
+    }
+}
